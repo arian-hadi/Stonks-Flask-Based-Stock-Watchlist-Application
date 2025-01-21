4b710c3d4de5bcb9413be07032a145dfc4f42c19
Arian Hadi <arianhadi2003@gmail.com>
2025-01-21 12:23:28 +0300

    Hello, world!

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')  # Add the '@' decorator here
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
