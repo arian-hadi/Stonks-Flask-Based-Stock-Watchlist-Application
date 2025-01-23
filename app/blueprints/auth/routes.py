fdbd8392444260f334dbb11ce924d6a534c51a53
Arian Hadi <arianhadi2003@gmail.com>
2025-01-23 23:38:30 +0300

    login & signup form setup

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,13 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request,redirect, url_for
+from app.forms import RegisterForm, LoginForm  
+
 
 signup_bp = Blueprint('signup_bp', __name__)
 login_bp = Blueprint('login_bp', __name__)
 
-@signup_bp.route('/signup')
-def signup():
-    return render_template("auth/signup.html")
+# @signup_bp.route('/signup', methods = ['GET', 'POST'])
+# def signup():
+#     return render_template("auth/signup.html")
 
+# @login_bp.route('/login')
+# def login():
+#     return render_template("auth/login.html")
 
-@login_bp.route('/login')
+@login_bp.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("auth/login.html")
+    form = LoginForm(request.form)
+    #if form.validate_on_submit():
+        # user = User.query.filter_by(email=form.email.data).first()
+        # if user is not None and user.verify_password(form.password.data):
+        #     login_user(user)
+        #     redirect_url = request.args.get('next') or url_for('main.login')
+        #     return redirect(redirect_url)
+    #pass
+    return render_template('auth/login.html', form=form)
+
+
+# @main.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('main.index'))
+
+
+@signup_bp.route('/signup', methods=["GET", "POST"])
+def register():
+    form = RegisterForm(request.form, csrf_enabled=True)
+    #if form.validate_on_submit():
+        # new_user = User(email=form.email.data,
+        #         username=form.username.data,
+        #         password=form.password.data)
+        # # db.session.add(new_user)
+        # # db.session.commit()
+        # return redirect(url_for('login_bp.login'))
+    return render_template('auth/signup.html', form=form)
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from .models import *
 
+
+#watchlist form
 class StockForm(FlaskForm):
     stock = StringField("Stock", validators=[DataRequired()])
     submit = SubmitField("Add Stock")
 
+
+#Authenticating forms
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=32)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=40)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=64)])
+    confirm = PasswordField('Verify password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Sign Up')
