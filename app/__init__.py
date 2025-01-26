0f96b63880b71a6d6b624f18534f8a871f65d8a7
Arian Hadi <arianhadi2003@gmail.com>
2025-01-26 14:58:02 +0300

    psotgres db setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app.config import Config
+from app.db_extention import db, migrate
  # Import the Blueprint
 from app.blueprints.main.routes import main_bp
 from app.blueprints.watchlist.routes import watchlist_bp
 from app.blueprints.auth.routes import signup_bp, login_bp
-from app.config import Config
+
 
 def create_app():
     app = Flask(__name__)
     
     app.config.from_object(Config)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    with app.app_context():
+        from app.models import User, Stock  # Import your models here
+        db.create_all()  # Ensure tables are created
+
+
 
     app.register_blueprint(main_bp) 
     app.register_blueprint(watchlist_bp, url_prefix='/watchlist')
--- app/config.py
+++ app/config.py
@@ -5,3 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', os.urandom(24))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
